cf7a1607ae51b5b3384830e8c814ff2ba864c094
Gordon Brüggemann <gb@gb-web.de>
2020-12-17 15:38:13 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-8.7.99',
+			'typo3' => '6.2.0-10.4.99',
 			'static_info_tables' => '6.3.9-'
 		),
 		'conflicts' => array(),
