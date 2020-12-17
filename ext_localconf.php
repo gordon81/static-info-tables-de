9ecb95200e164fa892a5ee1fafad2482c15354e5
Gordon Brüggemann <gb@gb-web.de>
2020-12-17 15:41:07 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
 defined('TYPO3_MODE') or die();
-
+$_EXTKEY = 'static_info_tables_de';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TypoScript/Extbase/setup.txt">');
